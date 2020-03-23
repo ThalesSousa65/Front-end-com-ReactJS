e5ad7327865a5056fc9582289aab6033efa7a2d3
Your Name <you@example.com>
2020-03-23 01:06:42 -0300

    first commit path

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Routes from "./routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./styles.css";
 
-export default App;
+import Header from "./components/Header";
+import Main from "./pages/main";
+
+const app = () => ( 
+      <div className="App">
+        <Header />
+        <Routes />
+      </div>
+    );
+
+export default app;
